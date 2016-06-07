7f805b05c3c79d9019010617ffc5139135fba899
Marc Monnerat <procrastinatio@gmail.com>
2016-06-07 16:06:48 +0200

    correct routing

--- print3/__init__.py
+++ print3/__init__.py
@@ -16,9 +16,11 @@ def main(global_config, **settings):
 
     # route definitions
     config.add_route('ogcproxy', '/ogcproxy')
-    config.add_route('print_create', '/print/geoadmin3/report.pdf')
-    config.add_route('print_progress', '/print/status/{id}.json')
-    config.add_route('print_cancel', '/print/cancel/{id}')
+
+    config.add_route('print_create', '/printmulti/create.json')
+    config.add_route('print_progress', '/printprogress')
+    config.add_route('print_cancel', '/printcancel')
+
     config.add_route('dev', '/dev')
     config.add_route('checker', '/checker')
     config.add_route('checker_dev', '/checker_dev')
