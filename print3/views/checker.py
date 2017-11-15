809ae86bd87d2f6b0b427d0fff9973abb5146523
Marc Monnerat <procrastinatio@gmail.com>
2017-11-15 10:49:41 +0000

    add checker and backend_checker

--- print3/views/checker.py
+++ print3/views/checker.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import requests
+from requests.exceptions import Timeout, ConnectionError, SSLError
+
 from pyramid.view import view_config
 from pyramid.response import Response
+from pyramid.httpexceptions import HTTPBadGateway, HTTPGatewayTimeout, HTTPServiceUnavailable
+
+req_session = requests.Session()
+req_session.mount('http://', requests.adapters.HTTPAdapter(max_retries=0))
 
 
 class Checker(object):
@@ -16,3 +23,23 @@ class Checker(object):
     @view_config(route_name='checker_dev')
     def dev(self):
         return Response(body='OK', status_int=200)
+
+    @view_config(route_name='backend_checker')
+    def dev(self):
+        settings = self.request.registry.settings
+        tomcat_port = settings.get('tomcat_port', 8010)
+        tomcat_url = 'http://localhost:{}'.format(tomcat_port)
+        content = ''
+
+        try:
+            r = req_session.get(tomcat_url,
+                    headers={'Referer': 'service print checker'},
+                    verify=False)
+            content = r.content
+        except (Timeout, SSLError, ConnectionError):
+            return HTTPBadGateway(detail='Cannot connect to backend tomcat')
+
+        if content == 'OK':
+            return Response(body='OK', status_int=200)
+
+        return HTTPServiceUnavailable(detail='Incomprehensible answer. tomcat is probably not ready yet.')
