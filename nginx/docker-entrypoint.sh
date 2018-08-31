91c8e1e4aef7e022d6ec7c82c19c341e6589fea9
Marc Monnerat <procrastinatio@gmail.com>
2018-08-31 11:19:52 +0000

    enabling logging post requests

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
 
-envsubst < nginx.conf.in > /etc/nginx/nginx.conf
+#envsubst < nginx.conf.in > /usr/local/openresty/nginx/conf/nginx.conf
+#envsubst < default.conf.in > /etc/nginx/conf.d/default.conf
+
+#cp openresty.conf /usr/local/openresty/nginx/conf/nginx.conf
+#envsubst "`printf '${%s} ' $(bash -c "compgen -A variable")`"  < default.conf.in > /etc/nginx/conf.d/default.conf
+envsubst "`printf '${%s} ' $(bash -c "compgen -A variable")`"  < nginx.conf.in > /usr/local/openresty/nginx/conf/nginx.conf
+#cp default.conf.in  /etc/nginx/conf.d/default.conf
 
 # Always put this damn shit
 exec "$@"
