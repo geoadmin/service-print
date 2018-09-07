c0646febf839e0a68acd122aef36750aef45c294
Marc Monnerat <procrastinatio@gmail.com>
2018-09-07 13:59:58 +0000

    cleanup

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 set -e
 
-#envsubst < nginx.conf.in > /usr/local/openresty/nginx/conf/nginx.conf
-#envsubst < default.conf.in > /etc/nginx/conf.d/default.conf
-
-#cp openresty.conf /usr/local/openresty/nginx/conf/nginx.conf
-#envsubst "`printf '${%s} ' $(bash -c "compgen -A variable")`"  < default.conf.in > /etc/nginx/conf.d/default.conf
+# Only replace variable that exists, nginx uses $ that must not be replaced
 envsubst "`printf '${%s} ' $(bash -c "compgen -A variable")`"  < nginx.conf.in > /usr/local/openresty/nginx/conf/nginx.conf
-#cp default.conf.in  /etc/nginx/conf.d/default.conf
 
 # Always put this damn shit
 exec "$@"
