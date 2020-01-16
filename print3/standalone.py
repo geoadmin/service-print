74d7e26a89905383ea95a7624c060c717bb410da
Marc Monnerat <procrastinatio@gmail.com>
2020-01-16 10:58:27 +0000

    dropping py2

--- print3/standalone.py
+++ print3/standalone.py
@@ -1,6 +1,5 @@
 import multiprocessing
 from gunicorn.app.base import BaseApplication
-from gunicorn.six import iteritems
 
 
 def number_of_workers():
@@ -15,10 +14,10 @@ class StandaloneApplication(BaseApplication):
         super(StandaloneApplication, self).__init__()
 
     def load_config(self):
-        config = dict([(key, value) for key, value in iteritems(self.options)
-                       if key in self.cfg.settings and value is not None])
-        for key, value in iteritems(config):
-            self.cfg.set(key.lower(), value)
+        config = {key: value for key, value in self.options.items()
+            if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)    
 
     def load(self):
         return self.application
--- print3/wsgi.py
+++ print3/wsgi.py
@@ -3,7 +3,6 @@
 import os
 import multiprocessing
 from gunicorn.app.base import BaseApplication
-from gunicorn.six import iteritems
 from print3.main import app as application
 
 
@@ -18,10 +17,10 @@ class StandaloneApplication(BaseApplication):
         self.application = app
         super(StandaloneApplication, self).__init__()
 
-    def load_config(self):
-        config = dict([(key, value) for key, value in iteritems(self.options)
-                       if key in self.cfg.settings and value is not None])
-        for key, value in iteritems(config):
+   def load_config(self):
+       config = {key: value for key, value in self.options.items()
+           if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
             self.cfg.set(key.lower(), value)
 
     def load(self):
